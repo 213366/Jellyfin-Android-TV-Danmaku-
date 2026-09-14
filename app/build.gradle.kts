daaf77d9d2304b32a704ecca34513513a8a20316
213366 <2916793125@qq.com>
2026-09-14 23:32:13 +0800

    build: name release artifact danmaku-jellyfintv

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,7 +97,7 @@ android {
 	}
 }
 
-base.archivesName.set("jellyfin-androidtv-v${project.getVersionName()}")
+base.archivesName.set("danmaku-jellyfintv-v${project.getVersionName()}")
 
 tasks.register("versionTxt") {
 	val path = layout.buildDirectory.asFile.get().resolve("version.txt")
